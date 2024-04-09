c30c19161206166b3d0d97ed1438ca45a2a6a805
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2024-04-10 00:54:01 +0530

    feat: added authorizatoin for chat apis

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
+import { UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 @Controller('chat')
 export class ChatController {
     constructor(private readonly chatService: ChatService) { }
@@ -10,8 +13,9 @@ export class ChatController {
         return this.chatService.generateText();
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
-    async create(@Body() query: string) {
+    async create(@Body() query: string, @Req() req){
         let message = await this.chatService.generateBasedOnQuery(query);
         return {message:message};
     }
